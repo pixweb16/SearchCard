50a178ce9e75fbeb9bf35859b24ac7d8647ae894
pixweb16 <pixweb@protonmail.com>
2021-02-12 23:35:18 +0100

    build
    files moved to root

---
+++ precache-manifest.9a6e5e6038347f70ba6b220e3ac73bea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad2175f0cce5752b721469f9c926119",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64ca8300ebc54c779612",
+    "url": "/static/css/main.033a0d12.chunk.css"
+  },
+  {
+    "revision": "40a2b0a4a7460f77a64f",
+    "url": "/static/js/2.06e152ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.06e152ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64ca8300ebc54c779612",
+    "url": "/static/js/main.b9443779.chunk.js"
+  },
+  {
+    "revision": "a2b5ba16d9eaeca8e528",
+    "url": "/static/js/runtime-main.3f7022a9.js"
+  },
+  {
+    "revision": "d3301b03af31fc6bc62399dcee622c4d",
+    "url": "/static/media/person.d3301b03.jpg"
+  }
+]);
